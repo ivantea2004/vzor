1150f66759d04aab4e5ce95f7ee8fdc0aa6d1bb8
Ivantea <ivantea2004@gmail.com>
2023-08-14 14:39:16 +0300

    add documentation generation
    Added simple documentation generation scripts.

--- include/vzor/decode_image.hpp
+++ include/vzor/decode_image.hpp
@@ -1,3 +1,14 @@
+
+/*!
+
+    \defgroup decoding
+
+    \brief decoding functions
+
+    Contains functions for image decoding
+
+*/
+
 #pragma once
 #include<cstdint>
 #include<vzor/detail/any_allocator.hpp>
@@ -20,6 +31,30 @@ namespace vzor
             );
     }
 
+    /*!
+    
+   
+
+        \ingroup decoding
+    
+        \brief Decodes image from memory
+
+        Decodes image from memory, allocates buffer for decoded image using specified allocator. 
+        Returns image's dimentions in out parameters.
+
+        \param [in] data Pointer to beginning of image file in memory
+        \param [in] size Size of image file in memory
+        \param [in] desired_components Count of pixel components, which decoded image should have
+        \param [out] width Width of decoded image
+        \param [out] height Height of decoded image
+        \param [out] real_components Count of pixel components of image in file
+        \param [in] allocator Allocator for image allocation and internal usage
+
+        \returns Pointer to allocated and decoded image
+
+        \remarks Throws any exception, which allocator throws
+
+    */
     template<typename Allocator = std::allocator<void>>
     std::uint8_t* decode_image(
             const void*data,
@@ -42,5 +77,10 @@ namespace vzor
                 alloc
             );
     }
+    /*!
+    
+        \example print_size.cpp
+
+    */
 
 }
